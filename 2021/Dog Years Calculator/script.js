b7f42134fba30940251cf03ccd673da942d661b0
grifficdesigns <74480900+grifficdesigns@users.noreply.github.com>
2022-01-15 18:48:24 +0000

    Create script.js

---
+++ 2021/Dog Years Calculator/script.js
@@ -0,0 +1,18 @@
+// input calculation
+function ageInDogYears() {
+  let myAge = document.getElementById("years").value;
+  let dogAge = myAge / 7;
+
+  // if under 1 year, convert to months
+  if (dogAge < 1) {
+    dogAge = ((Math.round(dogAge * 100) / 100) * 12).toFixed(0);
+    document.getElementById("result").innerHTML =
+      dogAge + " months old in dog years";
+
+    // 1 year and above
+  } else {
+    dogAge = Math.round(dogAge);
+    document.getElementById("result").innerHTML =
+      dogAge + " years old in dog years";
+  }
+}
